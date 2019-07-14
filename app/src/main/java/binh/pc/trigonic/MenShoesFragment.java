7c516abd04a1428b8e4881f509cd4c20661104d8
minhtus <mitu9811@gmail.com>
2019-07-14 23:15:57 +0700

    Clean up men and women tab; Using recyclerview; Adjust item layout

--- app/src/main/java/binh/pc/trigonic/MenShoesFragment.java
+++ app/src/main/java/binh/pc/trigonic/MenShoesFragment.java
@@ -19,17 +19,43 @@ import android.widget.Button;
 
 
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import binh.pc.trigonic.models.Product;
+import binh.pc.trigonic.models.ProductAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MenShoesFragment extends Fragment {
+    private List<Product> productList;
+    private ProductAdapter productAdapter;
+    private RecyclerView productRecyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_men_shoes, null);
-        final Intent intent = new Intent(getActivity(), DetailProductActivity.class);
-        Button button = view.findViewById(R.id.imagebutton);
-        button.setOnClickListener(v -> startActivity(intent));
+        View view = inflater.inflate(R.layout.fragment_men_shoes, container, false);
+//        final Intent intent = new Intent(getActivity(), DetailProductActivity.class);
+//        Button button = view.findViewById(R.id.imagebutton);
+//        button.setOnClickListener(v -> startActivity(intent));
+
+        fetchProducts();
+
+        productRecyclerView = view.findViewById(R.id.recycler_men);
+        productAdapter = new ProductAdapter(getContext(), productList);
+        productRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        productRecyclerView.setAdapter(productAdapter);
+
         return view;
+    }
+
+    private void fetchProducts() {
+        // TODO Input data for men here
+        Product p1 = new Product(R.drawable.nam720s, "Nike Air max 720 SATRN", "Black/White", "Men", 7450000, 8);
+        productList = new ArrayList<>(Arrays.asList(p1, p1, p1, p1, p1, p1));
     }
 
 }
--- app/src/main/java/binh/pc/trigonic/WomenShoesFragment.java
+++ app/src/main/java/binh/pc/trigonic/WomenShoesFragment.java
@@ -15,14 +15,41 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import binh.pc.trigonic.models.Product;
+import binh.pc.trigonic.models.ProductAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WomenShoesFragment extends Fragment {
+    private List<Product> productList;
+    private ProductAdapter productAdapter;
+    private RecyclerView productRecyclerView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_women_shoes, null);
+        View view = inflater.inflate(R.layout.fragment_women_shoes, container, false);
+
+        fetchProducts();
+
+        productRecyclerView = view.findViewById(R.id.recycler_women);
+        productAdapter = new ProductAdapter(getContext(), productList);
+        productRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+        productRecyclerView.setAdapter(productAdapter);
+
+        return view;
+    }
+
+    private void fetchProducts() {
+        // TODO input data for women here
+        Product p1 = new Product(R.drawable.aj1rhp, "Air Jordan 1 Retro Low Slip", "Black/White", "Men", 8500000, 9);
+        productList = new ArrayList<>(Arrays.asList(p1, p1, p1, p1, p1, p1));
     }
 
 }
