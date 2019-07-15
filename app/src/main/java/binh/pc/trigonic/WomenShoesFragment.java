2d42bd5ea5d84623ba15e72579c9df3add8b7ab1
phancongbinh1998 <40951217+phancongbinh1998@users.noreply.github.com>
2019-07-15 14:35:39 +0700

    modify data in men and women fragment

--- app/src/main/java/binh/pc/trigonic/WomenShoesFragment.java
+++ app/src/main/java/binh/pc/trigonic/WomenShoesFragment.java
@@ -48,8 +48,17 @@ public class WomenShoesFragment extends Fragment {
 
     private void fetchProducts() {
         // TODO input data for women here
-        Product p1 = new Product(R.drawable.aj1rhp, "Air Jordan 1 Retro Low Slip", "Black/White", "Men", 8500000, 9);
-        productList = new ArrayList<>(Arrays.asList(p1, p1, p1, p1, p1, p1));
+        Product p1 = new Product(R.drawable.aj1rls, "Air Jordan 1 Retro Low Slip", "Black/White", "Women", 8500000, 9);
+        Product p2 = new Product(R.drawable.aj1rhp, "Air Jodan 1 Retro High Premium", "Black/White", "Women", 8500000, 9);
+        Product p3 = new Product(R.drawable.nm2ktse, "Nike M2k Tenko SE", "Black/White", "Women", 8500000, 9);
+        Product p4 = new Product(R.drawable.nre55, "Nike React Element 55", "Black/White", "Women", 8500000, 9);
+        Product p5 = new Product(R.drawable.ncracp, "NikeCourt Royale AC", "Black/White", "Women", 8500000, 9);
+        Product p6 = new Product(R.drawable.namff720, "Nike Air Max FF 720", "Black/White", "Women", 8500000, 9);
+        Product p7 = new Product(R.drawable.nam90, "Nike Air Max 90", "Black/White", "Women", 8500000, 9);
+        Product p8 = new Product(R.drawable.nbmby, "Nike Blazer Mid By You", "Black/White", "Women", 8500000, 9);
+        Product p9 = new Product(R.drawable.nam270, "Nike Air Max 270", "Black/White", "Women", 8500000, 9);
+        Product p10 = new Product(R.drawable.ncclxf, "Air Jordan 1 Retro Low Slip", "Black/White", "Women", 8500000, 9);
+        productList = new ArrayList<>(Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10));
     }
 
 }
